3e58e261a95b398fcb5a034ee81fe1a41e0e7484
Ailton Filho <ailtonxdz@gmail.com>
2023-05-12 00:34:47 -0300

    adicionando JWT (Ainda em progresso)

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from livresse.views import UsersViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register(r'users', UsersViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
     path('', include(router.urls)),
 ]
--- livresse/views.py
+++ livresse/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from livresse.models import User
 from livresse.serializer import UserSerializer
 
 # Create your views here.
 
 class UsersViewSet(viewsets.ModelViewSet):
+    # permission_classes = (IsAuthenticated, )
     queryset = User.objects.all()
     serializer_class = UserSerializer
